d5a3085eeb613b8a25a37aedd2a63bb3c5d7289e
afters <afters.mail@gmail.com>
2010-09-02 09:39:38 -0700

    Escape quotes and don't escape backslash (immitating Mustache behaviour)

--- mustache.js
+++ mustache.js
@@ -247,8 +247,7 @@ var Mustache = function() {
       return s.replace(/&(?!\w+;)|["<>\\]/g, function(s) {
         switch(s) {
         case "&": return "&amp;";
-        case "\\": return "\\\\";
-        case '"': return '\"';
+        case '"': return "&quot;";
         case "<": return "&lt;";
         case ">": return "&gt;";
         default: return s;
