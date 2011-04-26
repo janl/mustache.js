9110fe006193c8a33a3ed48d632b5295c81eed64
Ben Cherry <bcherry@gmail.com>
2011-04-25 21:41:36 -0700

    remove the backslash escaping

--- mustache.js
+++ mustache.js
@@ -291,7 +291,6 @@ var Mustache = function() {
       return s.replace(/&(?!\w+;)|["'<>\\]/g, function(s) {
         switch(s) {
         case "&": return "&amp;";
-        case "\\": return "\\\\";
         case '"': return '&quot;';
         case "'": return '&#39;';
         case "<": return "&lt;";
