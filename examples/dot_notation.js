26e8ebe138b725778b2ef292fa93a7b14d75ba2c
Brandon Payton <brandon@happycode.net>
2011-10-29 21:34:40 -0700

    Replace tabs with 2 spaces.

--- examples/dot_notation.js
+++ examples/dot_notation.js
@@ -1,18 +1,18 @@
 var dot_notation = {
-	name: "A Book",
-	authors: ["John Power", "Jamie Walsh"],
-	price:{
-	  value: 200,
-	  vat: function() {
-	    return this.value * 0.2;
-	  },
-		currency: {
-			symbol: '&euro;',
-			name: 'Euro'
-		}
-	},
-	availability:{
-		status: true,
-	  text: "In Stock"
-	}
+  name: "A Book",
+  authors: ["John Power", "Jamie Walsh"],
+  price:{
+    value: 200,
+    vat: function() {
+      return this.value * 0.2;
+    },
+    currency: {
+      symbol: '&euro;',
+      name: 'Euro'
+    }
+  },
+  availability:{
+    status: true,
+    text: "In Stock"
+  }
 };
