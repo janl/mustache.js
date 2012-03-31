17bb32f1120778cfb0548921174d85c924275e53
Morgan Roderick <morgan@roderick.dk>
2012-03-31 12:43:39 +0200

    Porting re-use of enumerables test to Buster.JS

---
+++ test/reuse-of-enumerables-test.js
@@ -0,0 +1,34 @@
+/*jslint white:true, plusplus:true */
+/*global
+	buster,
+	assert,
+	Mustache
+*/
+(function(){
+	'use strict';
+	
+	buster.testCase('Re-use of enumerables', {
+		'must not consume data, it should be available to be re-used many times': function(){
+			var template = 
+					'{{#terms}}{{name}}, {{index}}, {{/terms}}' + '\n' +
+					'{{#terms}}{{name}}|{{index}}|{{/terms}}'
+				,
+				view = {
+					terms: [
+						{ name: 't1', index: 0 },
+						{ name: 't2', index: 1 }
+					]
+				},
+				expectedResult = 
+					't1, 0, t2, 1, ' + '\n' +
+					't1|0|t2|1|'
+				,
+				actualResult = Mustache.to_html( template, view );
+		
+			assert.equals( actualResult, expectedResult );
+		}
+	}); 
+}());
+
+
+
