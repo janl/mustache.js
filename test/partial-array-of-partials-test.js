16e5471ba7fcc0816750a4489c98d2800c4a4d9f
Morgan Roderick <morgan@roderick.dk>
2012-03-31 14:29:14 +0200

    Porting the partials array of partials test to Buster.JS

---
+++ test/partial-array-of-partials-test.js
@@ -0,0 +1,41 @@
+/*jslint white:true, plusplus:true */
+/*global
+	buster,
+	assert,
+	Mustache
+*/
+(function(){
+	'use strict';
+	
+	buster.testCase('Partial - array of partials', {
+	    'should allow the use of partials in sections': function(){
+			var template = [
+					'Here is some stuff!',
+					'{{#numbers}}',
+					'{{>partial}}',
+					'{{/numbers}}'
+				].join('\n'),
+				partial = '{{i}}\n',
+				view = {
+					numbers: [
+						{ i : '1'},
+						{ i : '2'},
+						{ i : '3'},
+						{ i : '4'}
+					]					
+				},
+				expectedResult =
+					'Here is some stuff!\n' +
+					'1\n' +
+					'2\n' +
+					'3\n' +
+					'4\n'
+				,
+				actualResult = Mustache.to_html( template, view, { partial : partial } );
+		
+	        assert.equals( actualResult, expectedResult );
+	    }
+	});	
+}());
+
+
