281336fc39e9b41e655bff2d4cdaab5501bc8fee
Morgan Roderick <morgan@roderick.dk>
2012-03-31 11:47:57 +0200

    Porting complex test case to Buster.JS

---
+++ test/complex-test.js
@@ -0,0 +1,62 @@
+/*jslint white:true, plusplus:true */
+/*global
+	buster,
+	assert,
+	Mustache
+*/
+(function(){
+	'use strict';
+	
+	buster.testCase('Complex', {
+		'should be able to process complex templates': function(){
+			var template = [
+					'<h1>{{header}}</h1>',
+					'{{#list}}',
+					'<ul>',
+					'{{#item}}',
+					'{{#current}}',
+					'<li><strong>{{name}}</strong></li>',
+					'{{/current}}',
+					'{{#link}}',
+					'<li><a href="{{url}}">{{name}}</a></li>',
+					'{{/link}}',
+					'{{/item}}',
+					'</ul>',
+					'{{/list}}',
+					'{{#empty}}',
+					'<p>The list is empty.</p>',
+					'{{/empty}}'
+				].join(''),
+				view = {
+					header: function() {
+						return "Colors";
+					},
+					item: [
+						{name: "red", current: true, url: "#Red"},
+						{name: "green", current: false, url: "#Green"},
+						{name: "blue", current: false, url: "#Blue"}
+					],
+					link: function() {
+						return this.current !== true;
+					},
+					list: function() {
+						return this.item.length !== 0;
+					},
+					empty: function() {
+						return this.item.length === 0;
+					}
+				},
+				expectedResult = [
+					'<h1>Colors</h1>',
+					'<ul>',
+					'<li><strong>red</strong></li>',
+					'<li><a href="#Green">green</a></li>',
+					'<li><a href="#Blue">blue</a></li>',
+					'</ul>'
+				].join(''),
+				actualResult = Mustache.to_html( template, view );
+		
+			assert.equals( actualResult, expectedResult );
+		}
+	}); 
+}());
