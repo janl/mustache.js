153d841ee1f6822461f85cc1b676a3b64fc4b47e
Morgan Roderick <morgan@roderick.dk>
2012-03-31 11:56:41 +0200

    Converting delimiters test to Buster.JS

---
+++ test/delimiters-test.js
@@ -0,0 +1,35 @@
+/*jslint white:true, plusplus:true */
+/*global
+	buster,
+	assert,
+	Mustache
+*/
+(function(){
+	'use strict';
+	
+	buster.testCase('Delimiters', {
+		'should do something': function(){
+			var template = [
+					'{{=<% %>=}}* <% first %>',
+					'* <% second %>',
+					'<%=| |=%>* | third |',
+					'|={{ }}=|* {{ fourth }}'
+				].join('\n'),
+				view = {
+					first: 'It worked the first time.',
+					second: 'And it worked the second time.',
+					third: 'Then, surprisingly, it worked the third time.',
+					fourth: 'Fourth time also fine!.'
+				},
+				expectedResult = [
+					'* It worked the first time.',
+					'* And it worked the second time.',
+					'* Then, surprisingly, it worked the third time.',
+					'* Fourth time also fine!.'
+				].join('\n'),
+				actualResult = Mustache.to_html( template, view );
+		
+			assert.equals( actualResult, expectedResult );
+		}
+	}); 
+}());
