61c9a6fcbcdbdc36afb868c04100d6ce0ce55c7c
Morgan Roderick <morgan@roderick.dk>
2012-03-31 12:27:36 +0200

    Porting simple test to Buster.JS

---
+++ test/simple-test.js
@@ -0,0 +1,38 @@
+/*jslint white:true, plusplus:true */
+/*global
+	buster,
+	assert,
+	Mustache
+*/
+(function(){
+	'use strict';
+	
+	buster.testCase('Simple', {
+	    'simple exercise of api': function(){
+			var template = 
+					'Hello {{name}}' + '\n' +
+					'You have just won ${{value}}!' + '\n' +
+					'{{#in_ca}}' +
+					'Well, ${{ taxed_value }}, after taxes.' +
+					'{{/in_ca}}'
+				,
+				view = {
+					name: "Chris",
+					value: 10000,
+					taxed_value: function() {
+						return this.value - (this.value * 0.4);
+					},
+					in_ca: true
+				},
+				expectedResult = [
+					'Hello Chris',
+					'You have just won $10000!',
+					'Well, $6000, after taxes.'			
+				].join('\n'),
+				actualResult = Mustache.to_html( template, view );
+		
+	        assert.equals( actualResult, expectedResult );
+	    }
+	});	
+}());
+
