ba367005c5f55f1193417ca66663471cfff18bac
Morgan Roderick <morgan@roderick.dk>
2012-03-31 14:03:44 +0200

    Porting inverted section test to Buster.JS

---
+++ test/inverted-section-test.js
@@ -0,0 +1,31 @@
+/*jslint white:true, plusplus:true */
+/*global
+	buster,
+	assert,
+	Mustache
+*/
+(function(){
+	'use strict';
+	
+	buster.testCase('Inverted section', {
+		// FIXME [Morgan Roderick]: split this into three different tests
+	    'should generate output for inverted sections': function(){
+			var template = [
+					'{{#repos}}<b>{{name}}</b>{{/repos}}',
+					'{{^repos}}No repos :({{/repos}}',
+					'{{^nothin}}Hello!{{/nothin}}'
+				].join('\n'),
+				view = {
+					repos : []					
+				},
+				expectedResult = [
+					'',
+					'No repos :(',
+					'Hello!'
+				].join('\n'),
+				actualResult = Mustache.to_html( template, view );
+		
+	        assert.equals( actualResult, expectedResult );
+	    }
+	});	
+}());
