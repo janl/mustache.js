e373a96c22753f22a1146bfd0beeaaec4a85727d
Morgan Roderick <morgan@roderick.dk>
2012-03-31 12:58:23 +0200

    Porting the partial-whitespace test to Buster.JS

---
+++ test/partial-whitespace-test.js
@@ -0,0 +1,51 @@
+/*jslint white:true, plusplus:true */
+/*global
+	buster,
+	assert,
+	Mustache
+*/
+(function(){
+	'use strict';
+	
+	buster.testCase('Partial - whitespace', {
+	    'should do something': function(){
+			var template = 
+					'<h1>{{  greeting  }}</h1>\n' + 
+					'{{> partial }}'+
+					'<h3>{{ farewell }}</h3>'
+				,
+				partial = 
+					'Hello {{ name}}\n' + 
+					'You have just won ${{value }}!\n' +
+					'{{# in_ca  }}' +
+					'Well, ${{ taxed_value }}, after taxes.\n' +
+					'{{/  in_ca }}'
+				,
+				view = {
+					greeting: function() {
+						return "Welcome";
+					},
+					farewell: function() {
+						return "Fair enough, right?";
+					},
+
+					name: "Chris",
+					value: 10000,
+					taxed_value: function() {
+						return this.value - (this.value * 0.4);
+					},
+					in_ca: true
+				},
+				expectedResult = [
+					'<h1>Welcome</h1>',
+					'Hello Chris',
+					'You have just won $10000!',
+					'Well, $6000, after taxes.',
+					'<h3>Fair enough, right?</h3>'
+				].join('\n'),
+				actualResult = Mustache.to_html( template, view, { partial : partial } );
+		
+	        assert.equals( actualResult, expectedResult );
+	    }
+	});	
+}());
