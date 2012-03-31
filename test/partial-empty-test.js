d0d8a5f31e1b7dc3647203e0f6ebf13cc90ee19f
Morgan Roderick <morgan@roderick.dk>
2012-03-31 13:35:45 +0200

    Porting partial - empty test to Buster.JS

---
+++ test/partial-empty-test.js
@@ -0,0 +1,23 @@
+/*jslint white:true, plusplus:true */
+/*global
+	buster,
+	assert,
+	Mustache
+*/
+(function(){
+	'use strict';
+	
+	buster.testCase('Partial - empty', {
+	    'should not generate any output for empty partials': function(){
+			var template = 'hey {{foo}}\n{{>partial}}',
+				partial = '',
+				view = {
+					  foo : 1
+				},
+				expectedResult = 'hey 1\n',
+				actualResult = Mustache.to_html( template, view, { partial : partial } );
+		
+	        assert.equals( actualResult, expectedResult );
+	    }
+	});	
+}());
