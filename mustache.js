2ba7481d41e356869dc2db695ccc4a0cf3ce38cd
David da Silva <dasilvacontin@gmail.com>
2014-12-20 16:54:11 +0100

    bump to version 1.0.0, resolves #325

--- mustache.js
+++ mustache.js
@@ -525,7 +525,7 @@
   };
 
   mustache.name = "mustache.js";
-  mustache.version = "0.8.2";
+  mustache.version = "1.0.0";
   mustache.tags = [ "{{", "}}" ];
 
   // All high-level mustache.* functions use this writer.
