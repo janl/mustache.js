378bcca8a5cfe4058f294a3dbb78e8755e8e0da5
Phillip Johnsen <johphi@gmail.com>
2015-11-17 09:14:29 +0100

    Improve HTML escaping.
    This closes a couple of potential exploit scenarios.
    Backtick (`) for older IEs and equals (=) for unquoted attributes.
    
    Refs https://github.com/wycats/handlebars.js/commit/83b8e846a3569bd366cf0b6bdc1e4604d1a2077e
    Closes https://github.com/janl/mustache.js/pull/388

--- mustache.js
+++ mustache.js
@@ -63,11 +63,13 @@
     '>': '&gt;',
     '"': '&quot;',
     "'": '&#39;',
-    '/': '&#x2F;'
+    '/': '&#x2F;',
+    '`': '&#x60;',
+    '=': '&#x3D;'
   };
 
   function escapeHtml (string) {
-    return String(string).replace(/[&<>"'\/]/g, function fromEntityMap (s) {
+    return String(string).replace(/[&<>"'`=\/]/g, function fromEntityMap (s) {
       return entityMap[s];
     });
   }
