492d6838a87b2d3bee79178f8e2f6c694dc4e162
Phillip Johnsen <johphi@gmail.com>
2019-12-07 12:06:16 +0100

    :ship: bump to version 3.2.0-beta.0

--- mustache.js
+++ mustache.js
@@ -651,7 +651,7 @@
 
   var mustache = {
     name: 'mustache.js',
-    version: '3.1.0',
+    version: '3.2.0-beta.0',
     tags: [ '{{', '}}' ],
     clearCache: undefined,
     escape: undefined,
