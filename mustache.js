4dc00b8a9c0857476bd41303477a89d249276373
urain39 <16981964+urain39@users.noreply.github.com>
2020-05-16 17:06:36 -0500

    Optimize `Writer.prototype.escapedValue` for numbers (#754)
    These changes optimizes the performance of escaping number
    values, by not passing them through the ordinary HTML escaping
    that is done on string values.

--- mustache.js
+++ mustache.js
@@ -657,7 +657,7 @@
   Writer.prototype.escapedValue = function escapedValue (token, context) {
     var value = context.lookup(token[1]);
     if (value != null)
-      return mustache.escape(value);
+      return typeof value === 'number' ? String(value) : mustache.escape(value);
   };
 
   Writer.prototype.rawValue = function rawValue (token) {
