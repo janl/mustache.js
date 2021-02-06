02c9ef1bf0c5f4fcef7fc3f7de559566be3bc66d
Phillip Johnsen <johphi@gmail.com>
2021-02-06 19:30:53 +0100

    Avoid use of `esm` when running tests on legacy Node.js versions
    Because `esm` does not support legacy versions of Node.js, at least not
    below Node.js 6 as of writing this, we have to avoid using that whenever
    tests are running on legacy versions.
    
    But now that the source code (`mustache.js`) is written in ESM syntax,
    how on earth are we going to run tests on these legacy versions of
    Node.js? We gotta run the build step first, so that we end up with a
    `mustache.js` file in CJS, or strictly speaking it will be UMD.
    
    That's kinda pain in the backside isn't it? Yes, but running tests on
    legacy versions are not meant to be done locally, but rather in CI. That
    means we can easily automate the flow of (1) building the source code
    before (2) starting the test suite.
    
    For our futureselves, if we want to stop running tests on legacy
    versions of Node.js; the changes introduces in this commit, could be
    removed completely.

--- test/helper.js
+++ test/helper.js
@@ -1,4 +1,12 @@
 var chai = require('chai');
+var nodejsMajorVersion = Number(process.versions.node.split(".")[0]);
+
+isLegacyNodeVersion = !(nodejsMajorVersion >= 10);
+
+if (!isLegacyNodeVersion) {
+  require = require("esm")(module);
+}
+
 assert = chai.assert;
 chai.should();
 Mustache = require('../mustache');
